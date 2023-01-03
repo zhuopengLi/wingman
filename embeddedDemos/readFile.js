3880d5e113e408cb6ba8bcef0db1095ce3c71573
Zhuopeng <zhuopeng.li@rwth-aachen.de>
2023-01-04 00:06:32 +0100

    Prune old files from merge

--- embeddedDemos/readFile.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-let rawdata = fs.readFileSync('AachenPipes.json');
-let student = JSON.parse(rawdata);
-console.log(student);
--- index.js
+++
@@ -1,5 +0,0 @@
-import {Server} from "./src/server/Server.js";
-import {BIMViewer} from "./src/BIMViewer.js";
-
-export {BIMViewer};
-export {Server};
