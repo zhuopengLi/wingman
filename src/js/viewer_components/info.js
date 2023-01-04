a39d759cd49be09f05ab75581aa092a8ca38528d
Zhuopeng <zhuopeng.li@rwth-aachen.de>
2023-01-04 15:32:27 +0100

    Minor fix

--- src/js/viewer_components/info.js
+++ src/js/viewer_components/info.js
@@ -1,10 +1,10 @@
 
 import { viewer } from "./viewer";
 import { models } from "../loader";
-import metaAachen from "../../data/metadata/aachen.json"
-import metaDutch from "../../data/metadata/schependomlaan.json"
-import metaDam from "../../data/metadata/waterlock.json"
-import metaKh from "../../data/metadata/westriversidehospital.json"
+// import metaAachen from "../../../public/assets/metadata/aachen.json"
+// import metaDutch from "../../../public/assets/metadata/schependomlaan.json"
+// import metaDam from "../../../public/assets/metadata/waterlock.json"
+// import metaKh from "../../../public/assets/metadata/westriversidehospital.json"
 
 var lastEntity = null;
 var lastColorize = null;
@@ -12,6 +12,36 @@ var lastColorize = null;
 let data
 let properties
 let info = document.querySelector("#right-panel")
+
+let metaAachen
+let metaDutch
+let metaDam
+let metaKh
+
+const fetchMeta = async url => {
+    let res = await fetch(url)
+    return await res.json()
+}
+
+fetchMeta("https://wingman-bim--api.cyclic.app/aachen")
+    .then(data => {
+        metaAachen = data
+    })
+
+fetchMeta("https://wingman-bim--api.cyclic.app/schependomlaan")
+    .then(data => {
+        metaDutch = data
+    })
+
+fetchMeta("https://wingman-bim--api.cyclic.app/waterlock")
+    .then(data => {
+        metaDam = data
+    })
+
+fetchMeta("https://wingman-bim--api.cyclic.app/hospital")
+    .then(data => {
+        metaKh = data
+    })
 
 viewer.cameraControl.on("picked", obj => {
 
@@ -31,7 +61,6 @@ viewer.cameraControl.on("picked", obj => {
             data = metaKh
             break
     }
-
 
     data.forEach(metas => {
         if (metas.id == entity.model.id.split("-")[1]) {
